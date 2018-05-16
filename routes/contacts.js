44f2487a49f06258007fccb05d41b5c32f09ef52
siddh4489 <siddh4489@gmail.com>
2018-05-16 22:06:40 +0530

    Update contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -25,9 +25,9 @@ router.get('/', async function(req, res, next) {
       // Get the first 10 contacts in alphabetical order
       // by given name
       const result = await client
-      .api('/me/people/?$search=siddh and filter ne ""')
+      .api('/me/people/?$search=siddh')
       .version("beta")
-      .top(10)
+      .top(1)
       .get();
 
       parms.contacts = result.value;
