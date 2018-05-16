da4eea2f2e20f5485ddb0a32a9933d70cc06de21
siddh4489 <siddh4489@gmail.com>
2018-05-16 17:32:49 +0530

    Update contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -30,7 +30,7 @@ router.get('/', async function(req, res, next) {
       .get();
 
       parms.contacts = result.value;
-      console.log('People--->'+JSON.stringify(result.value));
+      console.log('People--->'+JSON.stringify(result.value.emailAddresses));
       res.render('contacts', parms);
     } catch (err) {
       parms.message = 'Error retrieving contacts';
