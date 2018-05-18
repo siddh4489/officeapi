326b74d7fe1d8d7db3a847c063438dc0a1adbb74
siddh4489 <siddh4489@gmail.com>
2018-05-18 12:35:15 +0530

    Update contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -26,7 +26,7 @@ router.get('/', async function(req, res, next) {
       // by given name
   
       const result = await client
-      .api('/me/people/?$search=siddh')
+      .api('/me/people/?$search=sweta')
       .version("beta")
       .top(1)
       .get();
