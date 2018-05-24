a558e01a38372a7a10f72482c8bea1ebd4f981d3
siddh4489 <siddh4489@gmail.com>
2018-05-24 13:49:47 +0530

    Update event.js

--- routes/event.js
+++ routes/event.js
@@ -412,7 +412,7 @@ router.get('/', async function (req, res, next) {
 
             // Meeting Booking Validation
            if (this.starttime != undefined) { 
-            var postDataJSON = '{ "attendees": [ { "type": "required", "emailAddress": { "address": "' + this.mailto + '" } } ], "locationConstraint": { "isRequired": "false", "suggestLocation": "false", "locations": [ { "resolveAvailability": "false", "locationEmailAddress": "' + this.roomadd + '" } ] }, "timeConstraint": { "activityDomain":"work", "timeslots": [ { "start": { "dateTime": "' + this.starttime + '", "timeZone": "UTC" }, "end": { "dateTime": "' + this.endtime + '", "timeZone": "UTC" } } ] }, "meetingDuration": "PT60M", "returnSuggestionReasons": "false", "minimumAttendeePercentage": "100" }';
+            var postDataJSON = { "attendees": [ { "type": "required", "emailAddress": { "address":this.mailto} } ], "locationConstraint": { "isRequired": "false", "suggestLocation": "false", "locations": [ { "resolveAvailability": "false", "locationEmailAddress":this.roomadd} ] }, "timeConstraint": { "activityDomain":"work", "timeslots": [ { "start": { "dateTime": "' + this.starttime + '", "timeZone": "UTC" }, "end": { "dateTime":this.endtime, "timeZone": "UTC" } } ] }, "meetingDuration": "PT60M", "returnSuggestionReasons": "false", "minimumAttendeePercentage": "100" };
             console.log(' Iron Man ----->');
             var meetingresponse;
             const meetingResult = await client
